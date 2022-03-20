d41d309a0bf08751a70d3a8563dd0be13fce93fe
EC2 Default User <ec2-user@ip-172-31-2-101.ec2.internal>
2022-03-20 13:55:53 +0000

    Add top 新着商品

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_03_19_030136) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -40,7 +39,6 @@ ActiveRecord::Schema.define(version: 2022_03_19_030136) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -81,7 +79,6 @@ ActiveRecord::Schema.define(version: 2022_03_19_030136) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_valid", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
