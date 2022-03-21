ebd3eaeac8e0220127cdbda37a5a87a18bdc6961
EC2 Default User <ec2-user@ip-172-31-7-152.ec2.internal>
2022-03-21 06:42:10 +0000

    add user_update

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2022_03_19_030136) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.boolean "is_valid", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
