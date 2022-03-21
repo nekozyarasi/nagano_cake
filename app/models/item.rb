af9c099e9a5d44414bf9833229e69a9a3d9b263c
EC2 Default User <ec2-user@ip-172-31-29-163.ec2.internal>
2022-03-21 00:30:13 +0000

    add show fin

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
    has_one_attached :item_image
+   belongs_to :genre
+
+    def add_tax_price
+        (self.price * 1.10).round
+    end
+
+	validates :genre_id, :name, :price, presence: true
+	validates :introduction, length: {maximum: 200}
+	validates :price, numericality: { only_integer: true }
+
 end
