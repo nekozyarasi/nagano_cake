a7765719165c5d7b14d432a9624df54c84ae78d1
EC2 Default User <ec2-user@ip-172-31-7-152.ec2.internal>
2022-03-22 02:41:53 +0000

    add order index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.page(params[:page]).per(10)
+    @order_counts = a
   end
 
   def show
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+
+  belongs_to :Customer
+
+  has_many :OrderDetails, dependent: :destroy
+
 end
