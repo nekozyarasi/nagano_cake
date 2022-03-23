fbe6dbad60ce70f97f856c9ebdb908f5ea219c80
EC2 Default User <ec2-user@ip-172-31-7-152.ec2.internal>
2022-03-23 19:57:35 +0000

    add show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,5 +7,15 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    redirect_to admin_order_path(order)
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
 
   belongs_to :customer
 
-  enum blood_type: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
+  enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
 end
