9a381d6d9b9b32c5953d5453b3aabf2db5027c94
EC2 Default User <ec2-user@ip-172-31-2-101.ec2.internal>
2022-03-23 15:19:47 +0000

    Add cart_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
    has_one_attached :item_image
    belongs_to :genre
+   has_many :cart_items, dependent: :destroy
 
     def add_tax_price
         (self.price * 1.08).round
