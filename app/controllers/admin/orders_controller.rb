703058b777a86f00e8467d3758309c579bbc070b
EC2 Default User <ec2-user@ip-172-31-7-152.ec2.internal>
2022-03-24 18:04:55 +0000

    add order show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,12 +5,14 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @total_price = 0
+    # @total = @order_details.inject(0) { |sum, item| sum + sub_total }
   end
 
   def update
     order = Order.find(params[:id])
     order.update(order_params)
-    redirect_to admin_order_path(order)
+    redirect_to admin_order_path
   end
 
   private
