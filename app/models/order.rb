fb275605eeb14323478040b0260d568b2bf51996
EC2 Default User <ec2-user@ip-172-31-2-101.ec2.internal>
2022-03-24 07:50:48 +0000

    Add cart_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,12 @@
 class Order < ApplicationRecord
+    
+    belongs_to :customer
+    has_many :oeder_details, dependent: :destroy
+    
+    has_one_attached :item_image
+    
+    validates :post, :address, :name, :postage, :total_due, :payment, :order_status, presence: true
+    
+    enum payment: {credit_card:0, transfer:1}
+    enum order_status: {waiting_for_deposit:0, payment_confirmation:1, production:2, ready_to_ship:3, sent:4}
 end
