b79b5807d435988d01976a789dc98026a84f0773
EC2 Default User <ec2-user@ip-172-31-7-152.ec2.internal>
2022-03-24 05:37:56 +0000

    add order show

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,10 @@
 class Item < ApplicationRecord
    has_one_attached :item_image
    belongs_to :genre
-<<<<<<< HEAD
-   has_many :order_details
-=======
    has_many :cart_items
    has_many :orders, through: :order_details
    has_many :order_details
 
->>>>>>> origin/develop
 
     def add_tax_price
         (self.price * 1.08).round
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:datetime_jp] = '%Y/%m/%d %H:%M:%s'
+Time::DATE_FORMATS[:dateday_jp] = '%Y/%m/%d'
