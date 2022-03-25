ac376527494805ad017a2b458dbeb5c7dea8dbbf
EC2 Default User <ec2-user@ip-172-31-5-110.ec2.internal>
2022-03-25 05:03:19 +0000

    ログイン・退会に関する仕上げ完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  def after_inactive_sign_up_path_for(resource)
-      customer_customer_path(current_customer)
-  end
   def after_sign_in_path_for(resource)
     if resource.is_a?(Owner)
         admin_orders_path
     else
-        root_path
+        customer_customer_path(current_customer)
     end
   end
 
